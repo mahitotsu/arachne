6dd405a28be3f5ae923f13a054ea2a7c2e251238
akring <akio@mail.com>
2026-04-28 10:30:36 +0900

    style(customer-service): remove extra import spacing

--- food-delivery-demo/customer-service/src/test/java/com/mahitotsu/arachne/samples/delivery/customerservice/CustomerAccountRepositoryTest.java
+++ food-delivery-demo/customer-service/src/test/java/com/mahitotsu/arachne/samples/delivery/customerservice/CustomerAccountRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.mahitotsu.arachne.samples.delivery.customerservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
